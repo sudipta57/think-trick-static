1006f89249d71d28050e6da0abc880ce6438e49f
ThinkDevGroup <168450640+ThinkDevGroup@users.noreply.github.com>
2024-06-07 10:20:16 +0530

    conver to react

---
+++ ThinTrickCompany/src/components/About.jsx
@@ -0,0 +1,189 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <>
+    <div className="aboutarea__2 sp_top_30">
+    <div className="container">
+      <div className="row">
+        <div className="col-xl-6 col-lg-6 col-md-12 col-sm-12 col-12" data-aos="fade-up">
+          <div className="about__right__wraper__2">
+            <div className="educationarea__img" data-tilt>
+              <img loading="lazy" className="aboutarea__2__img__1" src="img/about/about_2.png" alt="education" />
+              <img loading="lazy" className="aboutarea__2__img__3" src="img/about/about_4.png" alt="education" />
+              <img loading="lazy" className="aboutarea__2__img__4" src="img/about/about_11.png" alt="education" />
+            </div>
+            
+          </div>
+        </div>
+        <div className="col-xl-5 col-lg-6 col-md-12 col-sm-12 col-12" data-aos="fade-up">
+          <div className="aboutarea__content__wraper">
+            <div className="aboutarea__button">
+              <div className="default__small__button">Who We Are</div>
+            </div>
+            <div className="aboutarea__headding heading__underline">
+              <h4>We Are All In One IT Solution & Technology Company</h4>
+            </div>
+            <div className="aboutarea__para aboutarea__para__2">
+            <p>Coming to the main reason you found us and believe me you have landed on the right website. We are premier IT Services and Digital Marketing Company in Kolkata and are happily serving the digital marketing needs and requirements of every size of business</p>
+            </div>
+              <h3>Our Values and Ethics</h3>
+              <li><span><i className="icofont-long-arrow-right" /></span>Rooted our business on high business ethics</li>
+            <li><span><i className="icofont-long-arrow-right" /></span>Transparency in every step</li>
+            <li><span><i className="icofont-long-arrow-right" /></span> The service is back up with proper documentation</li>
+            <li><span><i className="icofont-long-arrow-right" /></span> We work towards innovation to achieve excellence</li>
+            {/* <div className="aboutarea__list__2">
+              <ul>
+                <li>
+                  <i className="icofont-check" /> Lorem Ipsum is simply dummy
+                </li>
+                <li>
+                  <i className="icofont-check" /> Explore a variety of fresh educational teach
+                </li>
+                <li>
+                  <i className="icofont-check" /> Lorem Ipsum is simply dummy text of
+                </li>
+              </ul>
+            </div> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  {/* Learning__section__start */}
+  <div className="pricingarea sp_bottom_100 sp_top_90">
+    <div className="container">
+      <div className="row" data-aos="fade-up">
+        <div className="section__title text-center">
+          <div className="section__title__button">
+            <div className="default__small__button">Why Chose Us</div>
+          </div>
+          <div className="section__title__heading">
+            <h4>We Help You To Increase Your Sale Through Solutions
+            </h4>
+            <p>We are leading technology solutions providing company all over the world doing over 2 years</p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-xl-10 col-lg-4 col-md-6 col-sm-12 col-12" data-aos="fade-up">
+          <div className="pricingarea__content__wraper">
+            <div className="pricingarea__list">
+              <ul>
+                <li>
+                  <i className="icofont-check" />Fast and swift services
+                </li>
+                <li>
+                  <i className="icofont-check" />Reliable and trustworthy
+                </li><li>
+                  <i className="icofont-check" />Availability round the clock
+                </li><li>
+                  <i className="icofont-check" />100 % Customer satisfaction
+                </li><li>
+                  <i className="icofont-check" />Designing and developing the unique website
+                </li><li>
+                  <i className="icofont-check" />Affordable and pocket-friendly services
+                </li><li>
+                  <i className="icofont-check" />State-of-art facilities and Infrastructures
+                </li><li>
+                  <i className="icofont-check" />Helping you with the right strategies to get better ROI
+                </li><li>
+                  <i className="icofont-check" />A team of qualified professionals and digital marketers
+                </li><li>
+                  <i className="icofont-check" />The whole service cycle is backed up with proper documentation
+                </li><li>
+                  <i className="icofont-check" />Last but not least; we want to be your true companion on the path of your phenomenal success
+                </li>
+              </ul>
+            </div>
+            
+          </div>
+        </div>
+        {/* <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12" data-aos="fade-up">
+          <div className="pricingarea__content__wraper">
+            <div className="pricingarea__heading">
+              <div className="pricingarea__plan__type">
+                <h6>BASIC</h6>
+                <img loading="lazy" src="img/icon/price__2.png" alt />
+              </div>
+              <div className="pricingarea__number">
+                <h1><span className="price__currency">$</span>29<span className="price__durition">/
+                    month</span></h1>
+              </div>
+              <p>Perfect for startup</p>
+            </div>
+            <div className="pricingarea__list">
+              <ul>
+                <li>
+                  <i className="icofont-check" /> 5 user
+                </li>
+                <li>
+                  <i className="icofont-check" /> Learning Scope
+                </li>
+                <li>
+                  <i className="icofont-close close__button" /> Team collaboration
+                </li>
+                <li>
+                  <i className="icofont-check dark__color__2" /> Export HTML code
+                </li>
+                <li>
+                  <i className="icofont-check" /> Upload Your Logo
+                </li>
+              </ul>
+            </div>
+            <div className="pricingarea__button pricingarea__button__2">
+              <a className="default__button" href="#">Get Started</a>
+            </div>
+            <div className="pricingarea__text">
+              <p>No creadit card required</p>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12" data-aos="fade-up">
+          <div className="pricingarea__content__wraper">
+            <div className="pricingarea__heading">
+              <div className="pricingarea__plan__type">
+                <h6>PRO</h6>
+                <img loading="lazy" src="img/icon/price__3.png" alt />
+              </div>
+              <div className="pricingarea__number">
+                <h1><span className="price__currency">$</span>59<span className="price__durition">/
+                    month</span></h1>
+              </div>
+              <p>Perfect for startup</p>
+            </div>
+            <div className="pricingarea__list">
+              <ul>
+                <li>
+                  <i className="icofont-check" /> 2 user
+                </li>
+                <li>
+                  <i className="icofont-check" /> Learning Scope
+                </li>
+                <li>
+                  <i className="icofont-close close__button" /> Team collaboration
+                </li>
+                <li>
+                  <i className="icofont-check dark__color__2" /> Export HTML code
+                </li>
+                <li>
+                  <i className="icofont-check" /> Upload Your Logo
+                </li>
+              </ul>
+            </div>
+            <div className="pricingarea__button">
+              <a className="default__button" href="#">Get Started</a>
+            </div>
+            <div className="pricingarea__text">
+              <p>No creadit card required</p>
+            </div>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  </div>
+  {/* Learning__section__end*/}
+  </>
+  )
+}
---
+++ ThinTrickCompany/src/components/Footer.jsx
@@ -0,0 +1,163 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <>
+          {/* footer__section__start */}
+<div className="footerarea">
+  <div className="container">
+    {/* <div className="footerarea__newsletter__wraper">
+      <div className="row">
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" data-aos="fade-up">
+          <div className="footerarea__text">
+            <h3>Still You Need Our <span>Support</span> ?</h3>
+            <p>Don’t wait make a smart &amp; logical quote here. Its pretty easy.</p>
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12" data-aos="fade-up">
+          <div className="footerarea__newsletter">
+            <div className="footerarea__newsletter__input">
+              <form action="#">
+                <input type="text" placeholder="Enter your email here" />
+                <div className="footerarea__newsletter__button">
+                  <button type="submit" className="subscribe__btn">Subscribe Now</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div> */}
+    <div className="footerarea__wrapper footerarea__wrapper__2">
+      <div className="row">
+        <div className="col-xl-4 col-lg-4 col-md-6 col-sm-12" data-aos="fade-up">
+          <div className="footerarea__inner footerarea__about__us">
+            <div className="footerarea__heading">
+              <h3>About us</h3>
+            </div>
+            <div className="footerarea__content">
+              <p>orporate clients and leisure travelers has been relying on Groundlink for
+                dependable safe, and professional chauffeured car end service in major cities
+                across World.</p>
+            </div>
+            <div className="foter__bottom__text">
+              <div className="footer__bottom__icon">
+                <i className="icofont-clock-time" />
+              </div>
+              <div className="footer__bottom__content">
+                <h6>Opening Houres</h6>
+                <span>Mon - Sat(8.00 - 6.00)</span>
+                <span>Sunday - Closed</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-2 col-lg-2 col-md-6 col-sm-6" data-aos="fade-up">
+          <div className="footerarea__inner">
+            <div className="footerarea__heading">
+              <h3>Our Services</h3>
+            </div>
+            <div className="footerarea__list">
+              <ul>
+                <li>
+                  <a href="#">Web Development</a>
+                </li>
+                <li>
+                  <a href="#">Android App Development</a>
+                </li>
+                <li>
+                  <a href="#">Degital Marketing</a>
+                </li>
+                <li>
+                  <a href="#">Graphics And Logo Design </a>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-2 col-lg-2 col-md-6 col-sm-6" data-aos="fade-up">
+          <div className="footerarea__inner">
+            <div className="footerarea__heading">
+              <h3>Company</h3>
+            </div>
+            <div className="footerarea__list">
+              <ul>
+                <li>
+                  <a href="#">Home</a>
+                </li>
+                <li>
+                  <a href="#">About Us</a>
+                </li>
+                <li>
+                  <a href="#">Our Portfolio</a>
+                </li>
+                <li>
+                  <a href="#">Contact Us</a>
+                </li>
+               
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6" data-aos="fade-up">
+          <div className="footerarea__inner footerarea__padding__left">
+            <div className="footerarea__heading">
+              <h3>Contact Us</h3>
+            </div>
+            <div className="footerarea__list">
+              <ul>
+                <li>
+                  <a href="#">Ui Ux Design</a>
+                </li>
+                <li>
+                  <a href="#">Web Development</a>
+                </li>
+                <li>
+                  <a href="#">Business Strategy</a>
+                </li>
+                <li>
+                  <a href="#">Softwere Development</a>
+                </li>
+                <li>
+                  <a href="#">Business English</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="footerarea__copyright__wrapper footerarea__copyright__wrapper__2">
+      <div className="row">
+        <div className="col-xl-3 col-lg-3">
+          <div className="copyright__logo">
+            <a href="https://html.themewin.com/"><img loading="lazy" src="assets/img/logo/logo_2.png" alt="logo" /></a>
+          </div>
+        </div>
+        <div className="col-xl-6 col-lg-6">
+          <div className="footerarea__copyright__content footerarea__copyright__content__2">
+            <p>Copyright © <span>2024</span> by edurock. All Rights Reserved.</p>
+          </div>
+        </div>
+        <div className="col-xl-3 col-lg-3">
+          <div className="footerarea__icon footerarea__icon__2">
+            <ul>
+              <li><a href="http://facebook.com/"><i className="icofont-facebook" /></a></li>
+              <li><a href="http://twitter.com/"><i className="icofont-twitter" /></a></li>
+              <li><a href="http://vimeo.com/"><i className="icofont-vimeo" /></a></li>
+              <li><a href="http://linkedin.com/"><i className="icofont-linkedin" /></a></li>
+              <li><a href="http://skype.com/"><i className="icofont-skype" /></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+{/* footer__section__end */}
+ 
+
+    </>
+  )
+}
